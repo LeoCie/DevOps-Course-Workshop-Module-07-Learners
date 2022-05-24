6b76a57a87cf564403241f9e2df5dc6b29cb883d
Kris Gwynne <Kris.Gwynne@softwire.com>
2022-05-24 14:13:43 +0100

    Small visual change!

--- DotnetTemplate.Web/ViewModels/Home/FirstPageViewModel.cs
+++ DotnetTemplate.Web/ViewModels/Home/FirstPageViewModel.cs
@@ -12,7 +12,8 @@
             {
                 "Item 1",
                 "Item 2",
-                "Item 3"
+                "Item 3",
+                "Item 4"
             };
         }
     }
